d1fd0442b60ff2a271c91adabf29adc4e0e248c1
Levi Willmeth <lo9key@gmail.com>
2014-12-16 11:22:06 -0800

    Added some movement capabilities to Map, and began implementing placing walls using distance and angle of the turret.
    Also, fixed a bug (silly mistake) using Map.method insead of self.method().

--- slambot.py
+++ slambot.py
@@ -1,26 +1,47 @@
 """
 An early draft of creating and visualizing walls for the LBCC Slambot system.
 """
-import pygame, sys, random
+import pygame, sys, random, math
 from pygame.locals import *
 
 
 class Map:
     """ Base class for all maps. """
-    walls = {}
 
     def __init__(self, precision=5):
-        """ Defaults to precision to resolution of 5 cm. """
+        """ Constructor for Map class """
+        # Defaults to precision to resolution of 5 cm.
         self.precision = precision
+        # Floating point values holds bot position on map
+        self.posX, self.posY = 0.0, 0.0
+        # Dictionary of key:value pairs holds {(x,y):confidence} of walls
+        self.walls = {}
+
+    def find_wall(self, dist, angle):
+        """ Adds a wall using distance and angle of the turret. """
+        self.make_wall(dist*math.sin(angle), dist*math.cos(angle))
 
     def make_wall(self, x, y):
         """ Adds, or increments a map point. """
-        # Increment wall position, or start a new wall segment
-        if (x, y) in Map.walls:
-            Map.walls[(x, y)] += 1
+        # Bot position is a float but we want to store wall positions as
+        # integers to save space, so we round and truncate combined position.
+        x = int(round(self.posX+x))
+        y = int(round(self.posY+y))
+        # Increment existing wall position, or start a new wall segment
+        if (x, y) in self.walls:
+            self.walls[(x, y)] += 1
         else:
-            Map.walls[(x, y)] = 1
+            self.walls[(x, y)] = 1
 
+    def move_bot(self, x, y):
+        """ Move the bot relative to it's current position on the map. """
+        self.posX += x
+        self.posY += y
+
+    def set_bot(self, x, y):
+        """ Move the bot to a specific position on the map. """
+        self.posX = x
+        self.posY = y
 
 if __name__ == "__main__":
     pygame.display.init()
@@ -34,25 +55,28 @@ if __name__ == "__main__":
 
     # Add some randomized walls to the model
     for i in range(100):
-        localMap.make_wall(random.randrange(0, 600), random.randrange(0, 600))
+        if i % 2 == 0:
+            localMap.make_wall(random.choice([100, 400]), random.randrange(100, 400))
+        else:
+            localMap.make_wall(random.randrange(100, 400), random.choice([100, 400]))
 
     # Draw walls on a pygame map.
     for spot in localMap.walls:
-        color = 255-localMap.walls[spot]*55  # Walls get darker as they are seen more often
+        # Walls get darker as they are seen more often
+        color = min(abs(255-localMap.walls[spot]*55), 255)
         # Syntax is: screen, color, (x, y, width, height), thickness
         pygame.draw.rect(screen, (color, color, color),
-                         [a+b for a, b in zip(botPos, spot)]+[5, 5], 0)
+                         spot+(localMap.precision, localMap.precision), 0)
+        # Original design used a local botPos
+        # [a+b for a, b in zip(botPos, spot)]+[5, 5]
 
     # Redraw map using newly placed walls.
     pygame.display.update()
 
-
     # Pygame has stupid problems quitting
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print "Quitting."
                 pygame.quit()
                 sys.exit()
-                running = False
