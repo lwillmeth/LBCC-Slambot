d293af4483d48ca47b1ff79517db7eb7f87905a8
Levi Willmeth <lo9key@gmail.com>
2014-12-16 22:42:48 -0800

    Working on simulating measuring wall distances using a predetermined box.  This won't really be useful in the long run, but it's helping me better understand the process using radians.

--- slambot.py
+++ slambot.py
@@ -9,16 +9,17 @@ class Map:
     """ Base class for all maps. """
 
     def __init__(self, precision=5):
-        """ Constructor for Map class """
+        """ Constructor for Map class. """
         # Defaults to precision to resolution of 5 cm.
         self.precision = precision
         # Floating point values holds bot position on map
-        self.posX, self.posY = 0.0, 0.0
+        self.posX, self.posY = 0, 0
         # Dictionary of key:value pairs holds {(x,y):confidence} of walls
         self.walls = {}
+        self.botPos = [self.posX, self.posY]
 
     def find_wall(self, dist, angle):
-        """ Adds a wall using distance and angle of the turret. """
+        """ Add a wall using distance and angle (in radians) of the turret. """
         self.make_wall(dist*math.sin(angle), dist*math.cos(angle))
 
     def make_wall(self, x, y):
@@ -48,22 +49,27 @@ if __name__ == "__main__":
     # For some reason, using pygame.init() alone causes program to hang.
     # Could not find a better solution than initializing display by itself.
     screen = pygame.display.set_mode((500, 500))
-    screen.fill((255, 255, 255))  # white background
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    screen.fill(black)
 
     localMap = Map()
-    localMap.set_bot(250,250) # Position bot in center of the map.
+    localMap.set_bot(250, 250)  # Position bot in center of the room.
+    # Draw an empty room surrounded by walls.
+    # Syntax is: screen, color, (x, y, width, height), thickness
+    pygame.draw.rect(screen, white, (50, 50, 400, 400), 0)
+    pygame.draw.circle(screen, black, localMap.botPos, 4, 0)
 
-    # # Add some randomized walls to the model
-    # for i in range(100):
-    #     if i % 2 == 0:
-    #         localMap.make_wall(random.choice([100, 400]), random.randrange(100, 400))
-    #     else:
-    #         localMap.make_wall(random.randrange(100, 400), random.choice([100, 400]))
-
-    # Add better randomized walls using distance & angle instead.
-    for angle in range(0, 70, 5):
-        localMap.find_wall(200, math.radians(angle))
-
+    # Add randomized walls using distance & angle.
+    for angle in [math.radians(a) for a in range(0, 200, 5)]:
+        if (angle%(3*math.pi/4))/(math.pi/4) < 1:
+            d = abs(200/math.cos(angle))
+            print d, angle, 'cos'
+            localMap.find_wall(d, angle)
+        else:
+            d = abs(200/math.sin(angle))
+            print d, angle, 'sin'
+            localMap.find_wall(d, angle)
 
     # Draw walls on a pygame map.
     for spot in localMap.walls:
