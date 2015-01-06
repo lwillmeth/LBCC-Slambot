0558ff4e2a5b3e11d3bab3b094ac585d88b60d23
Levi Willmeth <lo9key@gmail.com>
2015-01-05 20:29:06 -0800

    Merged SlambotStepper.cpp into SlambotStepper.h to reduce complexity.  Began working on functions in SlambotMap.h, saving progress.

--- slambot_1_2/SlambotMap.h
+++ slambot_1_2/SlambotMap.h
@@ -2,35 +2,51 @@
 A library written for the LBCC Slambot on Jan 1, 2015, by Levi Willmeth.
 */
 #include "Arduino.h"
+#include <math.h>
+
 // Solves problems if library is imported more than once.
 #ifndef SlambotMap_h
   #define SlambotMap_h
 
+typedef struct wallCloud{
+  // Holds x,y positions of obstacles, and potentially other data.
+  unsigned int x;    // Big enough for a 2150 ft map
+  unsigned int y;
+  byte confidence;   // Number of times (0-255) this obstacle has been 'seen'
+};
+
 class SlambotMap{
   public:
     SlambotMap();  // We could allow them to set the map size.. but.. let's not.
-    void addPoint(unsigned long ix, unsigned long iy);
-
+    SlambotMap(unsigned int size);
+    void addCoord(unsigned int ix, unsigned int iy);
+    void addAngle(unsigned int theta, unsigned int distance);
   private:
-    mapPoint _allMaps;
-    unsigned int mapIndex; // 0-65,535 and after that we need to change data types.
+    wallCloud localMap[1000];
+    unsigned int mapIndex = 0; // 0-65,535 and after that we need to change data types.
 };
 
 SlambotMap::SlambotMap(){
-  _allMaps = mapPoint[1000];    // 1,000 readings should create a fairly large map.
-  unsigned int mapIndex = 0;    // redundant
+  // Constructor, empty for now.
 }
 
-typedef struct mapPoint{
-  /*
-  Holds x,y positions of obstacles, and potentially other data.
-  More thought needs to go into mapping wifi signals:
-    One option is to store the wifi node's mac address here.
-    Another, probably better, option is to make a seperate map just for wifi.
-  */
-  unsigned int x;
-  unsigned int y;
-  byte confidence;  // Number of times (0-255) this obstacle has been 'seen'
-};
+SlambotMap::SlambotMap(unsigned int size){
+  localMap = wallCloud[1000];
+}
 
+void SlambotMap::addCoord(unsigned int iX, unsigned int iY){
+  // Adds a barrier to localMap using an (x,y) pair.
+  localMap[mapIndex].x = iX;
+  localMap[mapIndex].y = iY;
+  localMap[mapIndex].confidence++;
+  mapIndex++;
+}
+
+void SlambotMap::addAngle(unsigned int theta, unsigned int distance){
+  // Adds a barrier to localMap using theta radians and a distance measurement.
+  localMap[mapIndex].x = distance*cos(theta);
+  localMap[mapIndex].y = distance*sin(theta);
+  localMap[mapIndex].confidence++;
+  mapIndex++;
+}
 #endif
--- slambot_1_2/slambot_1_2.ino
+++ slambot_1_2/slambot_1_2.ino
@@ -2,45 +2,20 @@
 First draft of the software controlling the LBCC SlamBot.
 See https://github.com/lo9key/LBCC-Slambot for commit history, bug notes, etc.
 */
-//#include <Stepper.h>
-//#include <AccelStepper.h>
 #include "SlambotStepper.h"
-
-typedef struct mapPoint{
-  /*
-  Holds x,y positions of obstacles, and potentially other data.
-  More thought needs to go into mapping wifi signals:
-    One option is to store the wifi node's mac address here.
-    Another, probably better, option is to make a seperate map just for wifi.
-  */
-  unsigned int x;
-  unsigned int y;
-  byte confidence;  // Number of times (0-255) this obstacle has been 'seen'
-};
-
-// Prepare a blank array to hold (temporary) local map values
-mapPoint localMap[1000];    // 1,000 readings should create a fairly large map.
-unsigned int mapIndex = 0;  // 0-65,535 and after that we need to change data types.
+#include "SlambotMap.h"
 
 // Prepare LIDAR turret
 // The 28YBJ-48 stepper motor pins must be in a 1-3-2-4 pattern, so use pins 8, 10, 9, 11.
 // 64 gear teeth * 32 steps in full step mode = 2048 positions
 SlambotStepper turret(2048, 8, 10, 9, 11);
-//Stepper turret(stepsPerRev, 8, 10, 9, 11);
-//AccelStepper turret(stepsPerRev, 8, 10, 9, 11);  // A halfstep stepper on pins 8-11 inc
 
 unsigned long rev_start_time;
-
 // Prepare LIDAR sensor
-unsigned long lidar_distance;
+unsigned int lidar_distance;
 
 void setup() {
   Serial.begin(9600);        // Initialize serial port
-  
-//  turret.setMaxSpeed(10);    // Max speed
-//  turret.setSpeed(5);        // Current speed
-//  turret.moveTo(20000);    // Sets goal
-
   pinMode(2, OUTPUT);        // Set pin 2 as trigger pin
   pinMode(3, INPUT);         // Set pin 3 as monitor pin
   digitalWrite(3, LOW);      // Set trigger LOW
@@ -50,9 +25,9 @@ void setup() {
 
 void loop() {
   rev_start_time = millis();
+//  while(turret.step())
   turret.step(2048);
   Serial.print("\nOne rev completed in ");
   Serial.print( (millis() - rev_start_time)/1000.0);
   Serial.print(" seconds.");
 }
-
